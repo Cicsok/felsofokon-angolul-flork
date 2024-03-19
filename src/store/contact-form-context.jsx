3a99b05ddcfdc8f0cd3d67f2ed26da644c43b2c4
SzakacsAgi <szakacs.agi21@gmail.com>
2024-03-19 15:13:48 +0100

    Test 12

--- src/store/contact-form-context.jsx
+++ src/store/contact-form-context.jsx
@@ -61,7 +61,8 @@ export default function ContactFormProvider({ children }) {
                     .then(response => {
                         contactFormStateDispatcher({ type: 'FORM_SENDING_FINISHED'});
                         console.log(response)
-                        if (response.statusCode === 200){
+                        console.log(response.body)
+                        if (response.status === 200){
                             contactFormStateDispatcher({ type: 'FORM_SUBMIT_SUCCESS' });
                         }
                         else{
